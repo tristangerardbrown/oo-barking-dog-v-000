ec6da837c9a379e9e4dcc262de5cd806ba1e4f30
Tristan Brown <tgbrown@stanford.edu>
2018-08-07 00:20:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
   def name=(name)
-    @name 
-  end 
+    @name
+  end
 
-  def name 
-    @name 
-  end 
+  def name
+    @name
+  end
 
   def bark
     puts "woof!"
-  end 
-  
+  end
+end 
